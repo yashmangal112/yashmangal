438942386623e53ada66be8f4b276e4929b306c7
Yash <=>
2022-09-18 21:53:17 +0530

    commit index html

--- Backend.js
+++ Backend.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || '3000';
 
 app.use(bodyParser.json())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({
     extended:true
 }))
