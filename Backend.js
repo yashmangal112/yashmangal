faf111969e775024fabcb1b0b52a6daca79fb929
Yash <=>
2022-09-18 09:58:23 +0530

    intial commit

---
+++ Backend.js
@@ -0,0 +1,149 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/Yash-Portfolio', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+var db = mongoose.connection;
+
+// db.once('connection', ()=>{
+//     console.log('Ah, we have our first user!');
+// })
+// db.on('error', ()=>{
+//     console.log('Sorry, Error in connecting to Database!')
+// })
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+
+//  Store Date For Main Site DB
+app.post('/submit', (req, res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var drop_line = req.body.drop_line;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "drop_line": drop_line
+    }
+
+    db.collection('users').insertOne(data, (err, collection)=>{
+        if (err) {
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    setTimeout(() => {
+        console.log('Site reload')
+        return res.redirect('index.html')    // changes
+    }, 3000);
+})
+
+
+//  Store Date For UI/UX Design Project DB
+app.post('/submit_Ux', (req, res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var drop_line = req.body.drop_line;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "drop_line": drop_line
+    }
+
+    db.collection('users_Ux').insertOne(data, (err, collection)=>{
+        if (err) {
+            throw err;
+        }
+        console.log("Record Inserted Successfully In UI/UX Design DB");
+    });
+
+    setTimeout(() => {
+        console.log('Site reload of UX Design Projects')
+        return res.redirect('UxDesignProjects.html')    // changes
+    }, 3000);
+})
+
+
+//  Store Date For Web Developer Project DB
+app.post('/submit_WebD', (req, res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var drop_line = req.body.drop_line;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "drop_line": drop_line
+    }
+
+    db.collection('users_WebD').insertOne(data, (err, collection)=>{
+        if (err) {
+            throw err;
+        }
+        console.log("Record Inserted Successfully In WebD Project DB");
+    });
+
+    setTimeout(() => {
+        console.log('Site reload of WebD Projects')
+        return res.redirect('WebDProjects.html')    // changes
+    }, 3000);
+})
+
+// app.get("/", (req, res)=>{
+//     res.set({
+//         "Allow-access-Allow-Origin": '*'
+//     })
+//     setTimeout(() => {
+//         console.log('Site reload')
+//       }, 3000);
+//     return res.redirect('index.html');
+// }).listen(3000)
+
+
+
+// const server = http.createServer((req, res)=>{
+//     res.setHeader('Content-Type', 'text-html', 'style-css')
+//     // res.yash();
+//     console.log(req.url)
+//     if (req.url == '/') {
+//         res.statusCode = 200;
+//         const index = fs.readFileSync('index.html');
+//         res.end(index.toString());
+//     }
+//     else if(req.url == '/UxDesignProjects.html'){
+//         res.statusCode = 200;
+//         const uxproject = fs.readFileSync('UxDesignProjects.html')
+//         res.end(uxproject.toString())
+//     }
+//     else if(req.url == '/WebDProjects.html'){
+//         res.statusCode = 200;
+//         const webdproject = fs.readFileSync('WebDProjects.html')
+//         res.end(webdproject.toString());
+//     }
+//     else{
+//         res.statusCode = 404;
+//         res.end('<h2>Not Found</h2>')
+//     }
+// })
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on port ${port}`)
+});
+// console.log('Server is listening on PORT 3000');
---
+++ Public/script.js
@@ -0,0 +1,66 @@
+console.log("js workmode on");
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+         if(this.scrollY > 80){
+            $('.navbar').addClass("sticky");
+         }
+         else{
+            $('.navbar').removeClass("sticky");
+         }
+         if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+         }else{
+            $('.scroll-up-btn').removeClass("show");
+         }
+    });
+    
+});
+
+
+// Script for submit btn
+const button = document.querySelector('.button');
+const submit = document.querySelector('.submit');
+
+function toggleClass() {
+	this.classList.toggle('running');
+}
+
+function addClass() {
+	this.classList.add('finished');
+}
+
+button.addEventListener('click', toggleClass);
+button.addEventListener('transitionend', toggleClass);
+button.addEventListener('transitionend', addClass);
+
+
+
+
+// toggle menu/navbar script
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});    
+
+var typed = new Typed(".typing", {
+    strings: ["Programmer","Developer","Designer","WebDeveloper","Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Programmer","Developer","Designer","WebDeveloper","Freelancer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+});
+
+
+
+
+
+
