d63d1a355684b876819b99e2a5966fca753edcbb
Yash <=>
2022-09-18 20:53:11 +0530

    commit

--- Backend.js
+++ Backend.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || '3000';
 
 app.use(bodyParser.json())
 app.use(express.static('public'))
